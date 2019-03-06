79561ab39c81dc9e5f8f2a4a681e8617c8fd2a20
Lisandro Ortega Gelder <lisandrogelder@gmail.com>
2019-03-05 19:33:51 -0500

    se agrega contenido a la clase Lan_Spanish

---
+++ application/views/Layouts/lan/Lan_Spanish.php
@@ -0,0 +1,15 @@
+<?php 
+	/**
+	 * clase de traducciones para idioma español
+	 */
+	class Lan_Spanish{
+		/**
+		 * metodo que devuelve aray de traducciones para 
+		 * cada vista de cada controlador
+		 * 
+		 * @return array de traducciones de cada vista
+		 */
+		public function get_translation(){
+		}
+	}
+?>
