c8a2fc3017548d11fafcb8758277561174acd300
Josue Martinez <josuermartinezm@gmail.com>
2019-03-07 01:10:44 -0400

    Modelos de organizacion, de posiciones o cargos y empleados, y modificacion del modelo de usuario.

--- application/controllers/userController.php
+++ application/controllers/userController.php
@@ -4,10 +4,10 @@ class userController extends Controller{
 		$this->action_index(new User(),true);
     }
     function create(){
-		$this->action_create(new User(),$_POST);
+		$this->action_create(new User(),$_POST,true);
     }
     function edit($id){
-		$this->action_edit($id,new User(),$_POST);
+		$this->action_edit($id,new User(),$_POST,true);
     }
     public function delete($id){
 		$this->action_delete($id,new User());
--- application/models/User.php
+++ application/models/User.php
@@ -1,13 +1,15 @@
 <?php
-//namespace App\Models;
+ //namespace App\Models;
 //use App\Models\Nivel_Usuario;
-class User extends Model{
-	
-	public function __construct(){
-	
+class User extends Model
+{
+
+	public function __construct()
+	{
+
 		parent::__construct('user');
-		$this->table_label='Users';
-		
+		$this->table_label = 'Users';
+
 		$this->add_columns(
 			array(
 				(new Column('id'))
@@ -17,31 +19,58 @@ class User extends Model{
 					->set_visible_grid(false)
 					->set_visible_form(false),
 
-				(new Column('username'))
-					->set_label('Nombre de Usuario')
+				(new Column('names'))
+					->set_label('Nombres')
 					->set_unike_key()
 					->set_name_key(),
-					
+
+				(new Column('lastnames'))
+					->set_label('Apellidos')
+					->set_unike_key()
+					->set_name_key(),
+
+				(new Column('mail'))
+					->set_label('E-Mail')
+					->set_type(Column::$COLUMN_TYPE_EMAIL)
+					->set_unike_key()
+					->set_name_key(),
+
+				(new Column('username'))
+					->set_label('Usuario')
+					->set_unike_key()
+					->set_name_key(),
+
 				(new Column('password'))
 					->set_label('Clave de Usuario')
 					->set_type(Column::$COLUMN_TYPE_PASS)
 					->set_visible_grid(false),
-				
+
 				(new Column('re_password'))
 					->set_label('Repita Clave')
 					->set_type(Column::$COLUMN_TYPE_PASS)
-					->set_table_field_name('clave_usuario')
+					->set_table_field_name('password_r')
 					->set_column_in_db(false)
 					->set_visible_grid(false),
-					
+
 				(new Column('user_level_id'))
 					->set_label('Nivel de Usuario')
 					->set_type(Column::$COLUMN_TYPE_SELECT)
-					->set_fk_entity(new User_Level())
+					->set_fk_entity(new User_Level()),
+
+				(new Column('employee_id'))
+					->set_label('Employee')
+					->set_type(Column::$COLUMN_TYPE_SELECT)
+					->set_fk_entity(new Employee()),
+				
+					(new Column('is_visitor'))
+					->set_label('Is Visitor?')
+					->set_type(Column::$COLUMN_TYPE_SELECT)
+					->set_values(array('Yes','No'))
+
 			)
-		);	
-		
+		);
+
 		$this->init();
 	}
 }
-?>
+ 
