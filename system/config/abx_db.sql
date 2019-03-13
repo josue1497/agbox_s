fa57f8264cff611309c24bdfeff8e83ee015bf36
Josue Martinez <jmartinezm@intelix.ad>
2019-03-13 16:42:55 -0400

    Commit Temp 2

---
+++ system/config/abx_db.sql
@@ -0,0 +1,407 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 13-03-2019 a las 20:41:44
+-- Versión del servidor: 5.6.34
+-- Versión de PHP: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `abx_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `affiliate`
+--
+
+CREATE TABLE `affiliate` (
+  `id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `approved` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `group`
+--
+
+CREATE TABLE `group` (
+  `id` int(11) NOT NULL,
+  `domain_id` int(11) NOT NULL,
+  `parent_group_id` int(11) DEFAULT NULL,
+  `name` varchar(60) NOT NULL,
+  `description` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for group''s information';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `group_user_role`
+--
+
+CREATE TABLE `group_user_role` (
+  `id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `menu`
+--
+
+CREATE TABLE `menu` (
+  `menu_id` int(5) NOT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  `description` varchar(256) DEFAULT NULL,
+  `icon` varchar(256) DEFAULT NULL,
+  `menu_order` int(5) DEFAULT NULL,
+  `url` varchar(256) DEFAULT NULL,
+  `parent_menu_id` int(5) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `menu`
+--
+
+INSERT INTO `menu` (`menu_id`, `title`, `description`, `icon`, `menu_order`, `url`, `parent_menu_id`) VALUES
+(7, 'Sources', 'Soruces', 'fab fa-500px', 0, 'source/index', 0),
+(9, 'Role', 'Role', 'fab fa-500px', NULL, 'role/index', 0),
+(10, 'Prueba', 'Hola soy un a prueba', 'fab fa-accessible-icon', 2, '#', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `note`
+--
+
+CREATE TABLE `note` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(140) NOT NULL,
+  `source_id` int(11) NOT NULL,
+  `summary` varchar(256) NOT NULL,
+  `agreement_type_id` int(11) NOT NULL,
+  `init_date` date DEFAULT NULL,
+  `finish_date` date DEFAULT NULL,
+  `status_id` int(11) NOT NULL,
+  `date_approved` date DEFAULT NULL,
+  `performer_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `note_approver`
+--
+
+CREATE TABLE `note_approver` (
+  `id` int(11) NOT NULL,
+  `note_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `choice` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `note_type`
+--
+
+CREATE TABLE `note_type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `description` varchar(140) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permission`
+--
+
+CREATE TABLE `permission` (
+  `id` int(5) NOT NULL,
+  `menu_id` int(5) NOT NULL,
+  `user_level_id` int(5) NOT NULL,
+  `can_read` varchar(3) NOT NULL,
+  `can_write` varchar(3) NOT NULL,
+  `can_edit` varchar(3) NOT NULL,
+  `can_delete` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `permission`
+--
+
+INSERT INTO `permission` (`id`, `menu_id`, `user_level_id`, `can_read`, `can_write`, `can_edit`, `can_delete`) VALUES
+(1, 5, 1, 'Yes', 'No', 'Yes', 'No');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `description` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for Role''s Information';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `source`
+--
+
+CREATE TABLE `source` (
+  `id` int(11) NOT NULL,
+  `title` varchar(60) NOT NULL,
+  `description` varchar(140) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `description` varchar(140) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  `names` varchar(140) NOT NULL,
+  `lastnames` varchar(140) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `user_level_id` int(11) NOT NULL,
+  `is_visitor` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `employee_id`, `names`, `lastnames`, `mail`, `username`, `password`, `user_level_id`, `is_visitor`) VALUES
+(1, 1, 'Administrador', 'Intelix', 'jmartinezm@intelix.biz', 'admin', 'admin', 1, 'N'),
+(2, 0, 'Josue ', 'Martinez', 'josuermartinezm@gmail.com', 'jmartinezm', 'josu12345', 1, 'No');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_level`
+--
+
+CREATE TABLE `user_level` (
+  `id` int(5) NOT NULL,
+  `name_level` varchar(50) DEFAULT NULL,
+  `access_level` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `user_level`
+--
+
+INSERT INTO `user_level` (`id`, `name_level`, `access_level`) VALUES
+(1, 'Administrador', 3),
+(2, 'Participante', 2);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `affiliate`
+--
+ALTER TABLE `affiliate`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `group`
+--
+ALTER TABLE `group`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `group_user_role`
+--
+ALTER TABLE `group_user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`menu_id`);
+
+--
+-- Indices de la tabla `note`
+--
+ALTER TABLE `note`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `note_approver`
+--
+ALTER TABLE `note_approver`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `note_type`
+--
+ALTER TABLE `note_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `permission`
+--
+ALTER TABLE `permission`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `source`
+--
+ALTER TABLE `source`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user_level`
+--
+ALTER TABLE `user_level`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `affiliate`
+--
+ALTER TABLE `affiliate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `group`
+--
+ALTER TABLE `group`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `group_user_role`
+--
+ALTER TABLE `group_user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `menu_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `note`
+--
+ALTER TABLE `note`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `note_approver`
+--
+ALTER TABLE `note_approver`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `note_type`
+--
+ALTER TABLE `note_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `permission`
+--
+ALTER TABLE `permission`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `source`
+--
+ALTER TABLE `source`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `user_level`
+--
+ALTER TABLE `user_level`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
