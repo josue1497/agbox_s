4e856f3dc37056e1c6b6b7b9d84b7810e031a69f
Josue Martinez <josuermartinezm@gmail.com>
2019-03-18 23:04:21 -0400

    Mezcla jmartinez

--- system/core/Application.php
+++ system/core/Application.php
@@ -14,7 +14,7 @@ define('MODELS_DIR', ROOT . "application/models/");
 define('DB_HOST','localhost');
 define('DB_NAME','abx_db');
 define('DB_USER','root');
-define('DB_PASS','12345678');
+define('DB_PASS','');
 
 
 require_once(ROOT.'system/core/Autoloader.php');
