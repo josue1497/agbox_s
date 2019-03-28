daa4e6f80ef9bf323922c76237ffc288ef04c63f
Lisandro Ortega Gelder <lisandrogelder@gmail.com>
2019-03-27 20:29:29 -0500

    se actualiza query console paa traer resultados de consltas en tabla de datos

--- application/controllers/query_consoleController.php
+++ application/controllers/query_consoleController.php
@@ -9,17 +9,15 @@ class query_consoleController extends Controller{
 	 * @return void
 	 */
     function index(){
-			$this->model = new User();
-      		$this->view = new View($this->model);
-			$this->view_processor = new ViewProcessor($this->view);
-
+			$this-> init(new User());
 			$this->model->table_label = 'Query Console';
+
 			$js="
 			$( document ).ready(function() {
 				console.log( 'document loaded' );
 				$('#btn_execute').on('click', function(sql){ 
 					var sql =document.getElementById('query_area').value;
-					var jqxhr = $.post( \"".SERVER_DIR.strtolower(CoreUtils::get_controller_name($this))."/execute_query\",
+					var jqxhr = $.post( \"".SERVER_DIR.strtolower(CoreUtils::get_controller_name($this))."/execute_update\",
                                 {sql:sql}, function(data, status) {
                         if(data!==\"1\"){
 							document.getElementById('alert_result').className='p-4 rounded bg-danger';
@@ -47,24 +45,23 @@ class query_consoleController extends Controller{
 	 * @return void
 	 */
     function select_query(){
-		$this->model = new User();
-		  $this->view = new View($this->model);
-		$this->view_processor = new ViewProcessor($this->view);
-
+		$this-> init(new User());
 		$this->model->table_label = 'Query Console';
 		$js="
 		$( document ).ready(function() {
 			console.log( 'document loaded' );
 			$('#btn_execute').on('click', function(sql){ 
-				var sql =document.getElementById('query_area').value;
+				var sql = document.getElementById('query_area').value;
 				var jqxhr = $.post( \"".SERVER_DIR.strtolower(CoreUtils::get_controller_name($this))."/execute_query\",
 							{sql:sql}, function(data, status) {
-					if(data!==\"1\"){
+					if(data=='0' || data ==0){
 						document.getElementById('alert_result').className='p-4 rounded bg-danger';
-						document.getElementById('alert_result').innerText=data;
+						document.getElementById('alert_result').innerText='No records for sql';
 					}else{
 						document.getElementById('alert_result').className='p-4 border rounded bg-success';
 						document.getElementById('alert_result').innerText='SQL SUCCESS!';
+						$('#result_sql').html(data);
+						/*document.getElementById('result_sql').innerHtml = data;*/
 					}
 									})
 					.fail(function() {
@@ -79,19 +76,57 @@ class query_consoleController extends Controller{
 		
 	}
 		
-		function execute_query(){
+		function execute_update(){
 			$query = explode(';',$_POST['sql']);
 			$result='';
 			foreach($query as $sql){
 				if(!empty($sql)){
-				$result = Model::execute_query($sql);
-				if(!$result){
-					echo "ERROR SYNTAX QUERY: --->".$sql;
-					break;
+					$result = Model::execute_update($sql);
+					if(!$result){
+						echo "ERROR SYNTAX QUERY: --->".$sql;
+						break;
+					}
 				}
 			}
-			}
 			echo $result;
+		}
+
+		function execute_query(){
+
+			$sql = $_POST['sql'];
+
+			if(!empty($sql))	{
+				$result = Model::execute_query($sql);
+				if($result){
+					$keys = array_keys($result[0]);
+					echo $this->build_table($keys,$result);
+					return;
+				}
+			}
+			echo '0';
+			return;
+		}
+
+		function build_table($keys,$result){
+			
+			$html = '<table class="table table-striped custab table-sm dataTable no-footer"><thead>';
+			
+			foreach($keys as $key){
+				$html.='<th>'.$key.'</th>';
+			}
+
+			$html.='</thead><tbody>';
+			foreach($result as $row){
+				$html.='<tr>';
+				foreach($keys as $key){
+					$html.= '<td>'.(isset($row[$key])?$row[$key]:'').'</td>'; 
+				}
+
+				$html.='</tr>';
+			}
+			$html.='</tbody></table>';
+
+			return $html;
 		}
     
     
