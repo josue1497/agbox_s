75aafeb49e439e97ce7c6a32253eaadbf22bc79b
Lisandro Ortega Gelder <lisandrogelder@gmail.com>
2019-04-03 16:52:38 -0500

    cambios en side.php

--- system/core/Application.php
+++ system/core/Application.php
@@ -2,7 +2,7 @@
 define('TEMPLATE_NAME','intelix_todo');
 define('APP_NAME','.:: A B X ::.');
 define("PATH_ROOT", dirname(__FILE__));
-define('APP_FOLDER','abx');
+define('APP_FOLDER','abx_app');
 define('VIEWS_DIR', ROOT . "application/views/");
 define('LAYOUT_DIR', VIEWS_DIR . "Layouts/");
 define('JS_DIR', LAYOUT_DIR . "scripts/");
@@ -18,7 +18,7 @@ define('SERVER_DIR', "http://".$_SERVER['SERVER_NAME'].SERVER_PORT."/".APP_FOLDE
 /* constantes de la db */
 define('DB_HOST','localhost');
 define('DB_NAME','abx_db');
-define('DB_USER','abxuser');
+define('DB_USER','root');
 define('DB_PASS','12345678');
 
 
