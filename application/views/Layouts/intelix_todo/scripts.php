e9a79ac721cbacc8bdd6d92155e3e4063b77c450
Josue Martinez <josuermartinezm@gmail.com>
2019-04-24 09:12:56 -0400

    Partial commit

--- application/views/Layouts/intelix_todo/scripts.php
+++ application/views/Layouts/intelix_todo/scripts.php
@@ -1,3 +1,62 @@
+<<<<<<< HEAD
+<!-- Bootstrap core JavaScript- - >
+<script src="{{ base_url }}application/views/Layouts/vendor/jquery/jquery.min.js"></script>
+<!--
+<script src="{{ base_url }}application/views/Layouts/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+-->
+<script src="{{ base_url }}application/views/Layouts/scripts/jquery.3.3.1.min.js"></script>
+<!-- jQuery CDN - - >
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<!-- Bootstrap CDN - - >
+<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+<!-- END CDN -->
+
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script> -->
+
+<script src="{{ base_url }}application/views/Layouts/scripts/popper.min.js"></script>
+
+<script src="{{ base_url }}application/views/Layouts/scripts/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{ base_url }}application/views/Layouts/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{ base_url }}application/views/Layouts/scripts/sb-admin-2.min.js"></script>
+
+<!-- Bootstrap-Iconpicker Bundle -->
+<script src="{{ base_url }}application/views/Layouts/vendor/icon-picker/js/bootstrap-iconpicker.bundle.min.js"></script>
+
+<script src="{{ base_url }}application/views/Layouts/scripts/functions.js"></script>
+
+<script src="{{ base_url }}application/views/Layouts/DataTables/datatables.js"></script>
+
+<script src="{{ base_url }}application/views/Layouts/scripts/select2.full.js"></script>
+
+<!-- <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script> -->
+
+<script src="{{ base_url }}application/views/Layouts/scripts/vue.js"></script>
+
+<script src="{{ base_url }}application/views/Layouts/scripts/axios.js"></script>
+
+<script type="text/javascript" src="{{ base_url }}application/views/Layouts/slick/slick.min.js"></script>
+
+<script>
+	$('.select2').select2({
+		placeholder: 'Select an option',
+		width: '100%',
+	});
+</script>
+
+<script>
+	$('.data-table').DataTable();
+</script>
+
+<script>
+	$(function() {
+		$('[data-toggle="tooltip"]').tooltip()
+	});
+=======
 <!-- Bootstrap core JavaScript- - >
 <script src="{{ base_url }}application/views/Layouts/vendor/jquery/jquery.min.js"></script>
 <!--
@@ -55,4 +114,5 @@
 	$(function() {
 		$('[data-toggle="tooltip"]').tooltip();
 	});
+>>>>>>> f4b1117673ee47d1b3d4ff684178754552d7ac46
 </script>
